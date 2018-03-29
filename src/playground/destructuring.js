18ebc6a46c12d6e5ac00fc55d44d213afcb65edb
Jupiter <jupiterbaudot@gmail.com>
2018-03-29 14:49:59 -0700

    Destructuring Arrays

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,32 +1,32 @@
 // console.log("destructuring")
 
+///// Destructuring Hashes
 
-//
-// const person = {
-//     // name: 'Jupiter',
-//     age: 33,
-//     location: {
-//         city: "Los Angeles",
-//         temp: "82"
-//     }
-// }
-//
-// // console.log(`${person.name} is ${person.age}` )
-//
-// // name is set to a default value if it doesn't exist
-// // const {name = "Anonymous", age} = person
-// const {name: firstname = "Anonymous", age} = person
-//
-//
-// console.log(`${firstname} is ${age}.` )
-//
-//
-// const {city, temp : temperature} = person.location
-//
-// if( city && temperature) {
-//     console.log(`${city} is ${temperature} degrees.` )
-// }
-//
+const person = {
+    // name: 'Jupiter',
+    age: 33,
+    location: {
+        city: "Los Angeles",
+        temp: "82"
+    }
+}
+
+// console.log(`${person.name} is ${person.age}` )
+
+// name is set to a default value if it doesn't exist
+// const {name = "Anonymous", age} = person
+const {name: firstname = "Anonymous", age} = person
+
+
+console.log(`${firstname} is ${age}.` )
+
+
+const {city, temp : temperature} = person.location
+
+if( city && temperature) {
+    console.log(`${city} is ${temperature} degrees.` )
+}
+
 
 
 const book = {
@@ -51,3 +51,15 @@ if (publisherName) {
 }
 
 
+
+
+//////// Destructuring Array
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+
+const [order = 'Coffee', , medium_price = '$1.00'] = item
+
+console.log(`A medium ${order} costs ${medium_price}`)
+
+
+// console.log(`A medium Coffee (hot) costs $2.50`)
